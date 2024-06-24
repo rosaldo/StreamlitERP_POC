91b2dffd41b2828ae6538b8d9d54850cd2817c43
Rosaldo <rosaldo@gmail.com>
2024-06-23 23:29:46 -0300

    v1.0.0 => initial skeleton

---
+++ database.py
@@ -0,0 +1,117 @@
+import os
+
+from sqlalchemy import (Boolean, Column, DateTime, Double, ForeignKey, Integer,
+                        String, create_engine)
+from sqlalchemy.engine import reflection
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Suppliers(Base):
+    version = "1.0.0"
+    __tablename__ = "suppliers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    status = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"Suppliers(id={self.id}, name={self.name}, email={self.email}, address={self.address}, phone={self.phone}, created_at={self.created_at}, updated_at={self.updated_at})"
+
+class Products(Base):
+    version = "1.0.0"
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    supplier = Column(String, nullable=False)
+    stock = Column(Double, nullable=False)
+    unit = Column(String, nullable=False)
+    price = Column(Double, nullable=False)
+    status = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"Products(id={self.id}, name={self.name}, description={self.description}, supplier={self.supplier}, stock={self.stock}, unit={self.unit}, price={self.price}, created_at={self.created_at}, updated_at={self.updated_at})"
+
+class Costumers(Base):
+    version = "1.0.0"
+    __tablename__ = "costumers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    status = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"Costumers(id={self.id}, name={self.name}, email={self.email}, address={self.address}, phone={self.phone}, created_at={self.created_at}, updated_at={self.updated_at})"
+
+class Sales(Base):
+    version = "1.0.0"
+    __tablename__ = "sales"
+
+    id = Column(Integer, primary_key=True)
+    costumer = Column(Integer, ForeignKey("costumers.id"), nullable=False)
+    price = Column(Double, nullable=False)
+    status = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"Sales(id={self.id}, costumer={self.costumer}, price={self.price}, created_at={self.created_at}, updated_at={self.updated_at})"
+
+class ProductsSales(Base):
+    version = "1.0.0"
+    __tablename__ = "products_sales"
+
+    id = Column(Integer, primary_key=True)
+    sale = Column(Integer, ForeignKey("sales.id"), nullable=False)
+    product = Column(Integer, ForeignKey("products.id"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    status = Column(Boolean, default=True, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+    def __repr__(self):
+        return f"ProductsSales(id={self.id}, sale={self.sale}, product={self.product}, quantity={self.quantity}, created_at={self.created_at}, updated_at={self.updated_at})"
+
+class Database:
+    version = "1.0.0"
+    suppliers = Suppliers()
+    products = Products()
+    costumers = Costumers()
+    sales = Sales()
+    products_sales = ProductsSales()
+
+    def __init__(self):
+        script_name = os.path.basename(__file__)[:-3]
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        db_name = f"sqlite:///{script_path}{os.sep}{script_name}.sql3"
+        self.engine = create_engine(db_name)
+        session = sessionmaker(bind=self.engine)
+        self.session = session()
+        inspector = reflection.Inspector.from_engine(self.engine)
+        if Suppliers.__tablename__ not in inspector.get_table_names():
+            Suppliers.__table__.create(self.engine)
+        if Products.__tablename__ not in inspector.get_table_names():
+            Products.__table__.create(self.engine)
+        if Costumers.__tablename__ not in inspector.get_table_names():
+            Costumers.__table__.create(self.engine)
+        if Sales.__tablename__ not in inspector.get_table_names():
+            Sales.__table__.create(self.engine)
+        if ProductsSales.__tablename__ not in inspector.get_table_names():
+            ProductsSales.__table__.create(self.engine)
+
+dbase = Database()
---
+++ home.py
@@ -0,0 +1,16 @@
+import os
+
+import pandas as pd
+import streamlit as st
+
+version = "1.0.0"
+ASSETS_PATH = "assets"
+
+st.set_page_config(page_title="Home")
+
+with open(os.path.join(ASSETS_PATH, "styles.css")) as css_file:
+    st.markdown(f"<style>{css_file.read()}</style>", unsafe_allow_html=True)
+
+st.logo(image="assets/logo.png", link="https://github.com/rosaldo")
+
+st.write("# Home")
